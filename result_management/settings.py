27b7cef685e91ac5e05ede277f053d1a887f0388
Mitun20 <mitung62@gmail.com>
2025-02-19 17:45:49 +0530

    setting changes

--- result_management/settings.py
+++ result_management/settings.py
@@ -95,14 +95,13 @@ DATABASES = {
 
    'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'result_management',
+        'NAME': 'result',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'KIT4compass!!!',
         'HOST': 'localhost',
-        'PORT': '3307',
+        'PORT': '',
         'OPTIONS': {
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        'charset': 'utf8mb4',
         },
         
     }
